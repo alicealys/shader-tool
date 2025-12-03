082f4b1ed438110904374b4e4ba0a1fe802ed07f
alice <58637860+alicealys@users.noreply.github.com>
2025-12-03 02:56:38 +0100

    cleanup disassembler + fix signature dumping

--- include/shader-tool/detail/disassembler.cpp
+++ include/shader-tool/detail/disassembler.cpp
@@ -4,155 +4,8 @@
 
 namespace alys::shader::detail
 {
-	void dump_operand(utils::string_writer& buffer, const operand_t& op)
+	void dump_operand_components(utils::string_writer& buffer, const operand_t& operand)
 	{
-		if (!op.extensions.empty())
-		{
-			for (const auto& extension : op.extensions)
-			{
-				switch (extension.modifier)
-				{
-				case D3D10_SB_OPERAND_MODIFIER_NEG:
-					buffer.write("-");
-					break;
-				case D3D10_SB_OPERAND_MODIFIER_ABS:
-					buffer.write("|");
-					break;
-				case D3D10_SB_OPERAND_MODIFIER_ABSNEG:
-					buffer.write("-|");
-					break;
-				}
-			}
-		}
-
-		const auto num_components = get_num_components(op.components.type);
-
-		switch (op.type)
-		{
-		case D3D10_SB_OPERAND_TYPE_IMMEDIATE32:
-		{
-			buffer.write("l(");
-			for (auto i = 0u; i < num_components; i++)
-			{
-				const auto type = std::fpclassify(op.immediate_values[i].float32);
-				if (type == FP_NORMAL)
-				{
-					buffer.write("%f", op.immediate_values[i].float32);
-				}
-				else
-				{
-					if (op.immediate_values[i].int32 == std::numeric_limits<std::int32_t>::min() ||
-						op.immediate_values[i].int32 == std::numeric_limits<std::int32_t>::max())
-					{
-						buffer.write("0x%X", op.immediate_values[i].uint32);
-					}
-					else
-					{
-						buffer.write("%i", op.immediate_values[i].int32);
-					}
-				}
-
-				if (i < num_components - 1)
-				{
-					buffer.write(", ");
-				}
-			}
-			buffer.write(")");
-			break;
-		}
-		case D3D10_SB_OPERAND_TYPE_IMMEDIATE64:
-		{
-			const auto n = num_components == 1 ? 1 : 2;
-			buffer.write("d(");
-			for (auto i = 0u; i < n; i++)
-			{
-				const auto type = std::fpclassify(op.immediate_values[i].float64);
-				if (type == FP_NORMAL)
-				{
-					buffer.write("%fl", op.immediate_values[i].float64);
-				}
-				else
-				{
-					buffer.write("%lli", op.immediate_values[i].int64);
-				}
-
-				if (i < n - 1)
-				{
-					buffer.write(", ");
-				}
-			}
-			buffer.write(")");
-			break;
-		}
-		default:
-		{
-			const auto iter = operand_names.find(op.type);
-			if (iter != operand_names.end())
-			{
-				buffer.write(iter->second);
-			}
-		}
-		}
-
-		const auto dump_index = [&](const std::uint32_t index)
-		{
-			switch (op.indices[index].representation)
-			{
-			case D3D10_SB_OPERAND_INDEX_IMMEDIATE32:
-				buffer.write("%i", op.indices[index].value.uint32);
-				break;
-			case D3D10_SB_OPERAND_INDEX_IMMEDIATE64:
-				buffer.write("%lli", op.indices[index].value.uint64.value);
-				break;
-			case D3D10_SB_OPERAND_INDEX_RELATIVE:
-				dump_operand(buffer, *op.indices[index].extra_operand);
-				buffer.write(" + 0");
-				break;
-			case D3D10_SB_OPERAND_INDEX_IMMEDIATE32_PLUS_RELATIVE:
-				dump_operand(buffer, *op.indices[index].extra_operand);
-				buffer.write(" + %i", op.indices[index].value.uint32);
-				break;
-			case D3D10_SB_OPERAND_INDEX_IMMEDIATE64_PLUS_RELATIVE:
-				dump_operand(buffer, *op.indices[index].extra_operand);
-				buffer.write(" + %lli", op.indices[index].value.uint64.value);
-				break;
-			}
-		};
-
-		if (op.dimension >= D3D10_SB_OPERAND_INDEX_1D)
-		{
-			const auto add_brackets = 
-				op.type == D3D11_SB_OPERAND_TYPE_INPUT_CONTROL_POINT ||
-				op.type == D3D11_SB_OPERAND_TYPE_OUTPUT_CONTROL_POINT || 
-				op.indices[0].representation >= D3D10_SB_OPERAND_INDEX_RELATIVE;
-
-			if (add_brackets)
-			{
-				buffer.write("[");
-			}
-
-			dump_index(0);
-
-			if (add_brackets)
-			{
-				buffer.write("]");
-			}
-		}
-
-		if (op.dimension >= D3D10_SB_OPERAND_INDEX_2D)
-		{
-			buffer.write("[");
-			dump_index(1);
-			buffer.write("]");
-		}
-
-		if (op.dimension == D3D10_SB_OPERAND_INDEX_3D)
-		{
-			buffer.write("[");
-			dump_index(2);
-			buffer.write("]");
-		}
-
 		const auto print_component = [&](const std::uint32_t component)
 		{
 			switch (component)
@@ -172,22 +25,14 @@ namespace alys::shader::detail
 			}
 		};
 
-#ifdef PRINT_DETAILS
-		buffer.write("(%i)", op.components.type);
-#endif
-
-		switch (op.components.selection_mode)
+		switch (operand.components.selection_mode)
 		{
 		case D3D10_SB_OPERAND_4_COMPONENT_MASK_MODE:
 		{
-			const auto mask = (op.components.mask << D3D10_SB_OPERAND_4_COMPONENT_MASK_SHIFT);
+			const auto mask = (operand.components.mask << D3D10_SB_OPERAND_4_COMPONENT_MASK_SHIFT);
 			if ((mask & D3D10_SB_OPERAND_4_COMPONENT_MASK_MASK) != 0)
 			{
-#ifdef PRINT_DETAILS
-				buffer.write(".mask_");
-#else
 				buffer.write(".");
-#endif
 			}
 
 			if (mask & D3D10_SB_OPERAND_4_COMPONENT_MASK_X)
@@ -214,40 +59,204 @@ namespace alys::shader::detail
 		}
 		case D3D10_SB_OPERAND_4_COMPONENT_SWIZZLE_MODE:
 		{
-#ifdef PRINT_DETAILS
-			buffer.write(".swz_");
-#else
 			buffer.write(".");
-#endif
-			print_component(op.components.names[0]);
-			print_component(op.components.names[1]);
-			print_component(op.components.names[2]);
-			print_component(op.components.names[3]);
+			print_component(operand.components.names[0]);
+			print_component(operand.components.names[1]);
+			print_component(operand.components.names[2]);
+			print_component(operand.components.names[3]);
 			break;
 		}
 		case D3D10_SB_OPERAND_4_COMPONENT_SELECT_1_MODE:
 		{
-#ifdef PRINT_DETAILS
-			buffer.write(".sel_");
-#else
 			buffer.write(".");
-#endif
-			print_component(op.components.names[0]);
+			print_component(operand.components.names[0]);
+			break;
+		}
+		}
+	}
+
+	void dump_operand_index(utils::string_writer& buffer, const operand_t& operand, const std::uint32_t index, bool add_brackets)
+	{
+		if (add_brackets)
+		{
+			buffer.write("[");
+		}
+
+		switch (operand.indices[index].representation)
+		{
+		case D3D10_SB_OPERAND_INDEX_IMMEDIATE32:
+			buffer.write("%i", operand.indices[index].value.uint32);
+			break;
+		case D3D10_SB_OPERAND_INDEX_IMMEDIATE64:
+			buffer.write("%lli", operand.indices[index].value.uint64.value);
+			break;
+		case D3D10_SB_OPERAND_INDEX_RELATIVE:
+			dump_operand(buffer, *operand.indices[index].extra_operand);
+			buffer.write(" + 0");
+			break;
+		case D3D10_SB_OPERAND_INDEX_IMMEDIATE32_PLUS_RELATIVE:
+			dump_operand(buffer, *operand.indices[index].extra_operand);
+			buffer.write(" + %i", operand.indices[index].value.uint32);
+			break;
+		case D3D10_SB_OPERAND_INDEX_IMMEDIATE64_PLUS_RELATIVE:
+			dump_operand(buffer, *operand.indices[index].extra_operand);
+			buffer.write(" + %lli", operand.indices[index].value.uint64.value);
+			break;
+		}
+
+		if (add_brackets)
+		{
+			buffer.write("]");
+		}
+	}
+
+	void dump_operand_indices(utils::string_writer& buffer, const operand_t& operand)
+	{
+		if (operand.dimension >= D3D10_SB_OPERAND_INDEX_1D)
+		{
+			const auto add_brackets =
+				operand.type == D3D11_SB_OPERAND_TYPE_INPUT_CONTROL_POINT ||
+				operand.type == D3D11_SB_OPERAND_TYPE_OUTPUT_CONTROL_POINT ||
+				operand.indices[0].representation >= D3D10_SB_OPERAND_INDEX_RELATIVE;
+
+			dump_operand_index(buffer, operand, 0, add_brackets);
+		}
+
+		if (operand.dimension >= D3D10_SB_OPERAND_INDEX_2D)
+		{
+			dump_operand_index(buffer, operand, 1, true);
+		}
+
+		if (operand.dimension == D3D10_SB_OPERAND_INDEX_3D)
+		{
+			dump_operand_index(buffer, operand, 2, true);
+		}
+	}
+
+	void dump_operand_immediate32(utils::string_writer& buffer, const operand_t& operand)
+	{
+		const auto num_components = get_num_components(operand.components.type);
+
+		buffer.write("l(");
+
+		for (auto i = 0u; i < num_components; i++)
+		{
+			const auto type = std::fpclassify(operand.immediate_values[i].float32);
+			if (type == FP_NORMAL)
+			{
+				buffer.write("%f", operand.immediate_values[i].float32);
+			}
+			else
+			{
+				if (operand.immediate_values[i].int32 == std::numeric_limits<std::int32_t>::min() ||
+					operand.immediate_values[i].int32 == std::numeric_limits<std::int32_t>::max())
+				{
+					buffer.write("0x%X", operand.immediate_values[i].uint32);
+				}
+				else
+				{
+					buffer.write("%i", operand.immediate_values[i].int32);
+				}
+			}
+
+			if (i < num_components - 1)
+			{
+				buffer.write(", ");
+			}
+		}
+
+		buffer.write(")");
+	}
+
+	void dump_operand_immediate64(utils::string_writer& buffer, const operand_t& operand)
+	{
+		const auto num_components = get_num_components(operand.components.type);
+		const auto n = num_components == 1 ? 1 : 2;
+
+		buffer.write("d(");
+
+		for (auto i = 0u; i < n; i++)
+		{
+			const auto type = std::fpclassify(operand.immediate_values[i].float64);
+			if (type == FP_NORMAL)
+			{
+				buffer.write("%fl", operand.immediate_values[i].float64);
+			}
+			else
+			{
+				buffer.write("%lli", operand.immediate_values[i].int64);
+			}
+
+			if (i < n - 1)
+			{
+				buffer.write(", ");
+			}
+		}
+
+		buffer.write(")");
+	}
+
+	void dump_operand_name(utils::string_writer& buffer, const operand_t& operand)
+	{
+		const auto iter = operand_names.find(operand.type);
+
+		if (iter != operand_names.end())
+		{
+			buffer.write(iter->second);
+		}
+		else
+		{
+			buffer.write("unknown");
+		}
+	}
+
+	void dump_operand(utils::string_writer& buffer, const operand_t& operand)
+	{
+		for (const auto& extension : operand.extensions)
+		{
+			switch (extension.modifier)
+			{
+			case D3D10_SB_OPERAND_MODIFIER_NEG:
+				buffer.write("-");
+				break;
+			case D3D10_SB_OPERAND_MODIFIER_ABS:
+				buffer.write("|");
+				break;
+			case D3D10_SB_OPERAND_MODIFIER_ABSNEG:
+				buffer.write("-|");
+				break;
+			}
+		}
+
+		switch (operand.type)
+		{
+		case D3D10_SB_OPERAND_TYPE_IMMEDIATE32:
+		{
+			dump_operand_immediate32(buffer, operand);
+			break;
+		}
+		case D3D10_SB_OPERAND_TYPE_IMMEDIATE64:
+		{
+			dump_operand_immediate64(buffer, operand);
+			break;
+		}
+		default:
+		{
+			dump_operand_name(buffer, operand);
+			dump_operand_indices(buffer, operand);
+			dump_operand_components(buffer, operand);
 			break;
 		}
 		}
 
-		if (!op.extensions.empty())
+		for (const auto& extension : operand.extensions)
 		{
-			for (const auto& extension : op.extensions)
+			switch (extension.modifier)
 			{
-				switch (extension.modifier)
-				{
-				case D3D10_SB_OPERAND_MODIFIER_ABS:
-				case D3D10_SB_OPERAND_MODIFIER_ABSNEG:
-					buffer.write("|");
-					break;
-				}
+			case D3D10_SB_OPERAND_MODIFIER_ABS:
+			case D3D10_SB_OPERAND_MODIFIER_ABSNEG:
+				buffer.write("|");
+				break;
 			}
 		}
 	}
